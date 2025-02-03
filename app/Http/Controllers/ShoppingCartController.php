d24e918a9f8917daaa82dd84706ddb100cf85482
dukeduke00 <dusangostic69@gmail.com>
2025-02-03 11:38:14 +0100

    * Homework for Laravel #23

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CartAddRequest;
+use App\Models\ProductModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
@@ -12,14 +13,39 @@ class ShoppingCartController extends Controller
 
     public function index()
     {
+//        $allProducts = [];
+//
+//        foreach(Session::get('product') as $cartItem)
+//        {
+//            $allProducts[] = $cartItem['product_id'];
+//        }
+//
+//        // $allProducts = [2, 4, 5]...
+//        // SELECT * FROM products WHERE id IN ($allProducts)
+//
+//        $products = ProductModel::whereIn('id', $allProducts)->get();*/
+
+        $allProducts = array_column(Session::get('product'), 'product_id');
+        $products = ProductModel::whereIn('id', $allProducts)->get();
+
         return view('cart', [
-            'cart' => Session::get('product')
+            'cart' => Session::get('product'),
+            'products' => $products,
         ]);
     }
     public function addToCart(CartAddRequest $request)
     {
-        Session::put('product', [
-            $request->id => $request->amount
+        $product = ProductModel::where(['id' => $request->id])->first();
+
+        if($product->amount < $request->amount)
+        {
+            return redirect()->back();
+        }
+
+        Session::push('product', [
+            'product_id' => $request->id,
+            'amount' => $request->amount,
+
         ]);
 
         return redirect()->route('cart.all');
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,9 +2,17 @@
 
 @section("sadrzajStranice")
 
- @foreach($cart as $product => $amount)
 
-     {{ $product. " ".$amount}}
+ @foreach($products as $product)
+     @foreach($cart as $item)
+         @if($item['product_id'] == $product->id)
+             <p>Ime proizvoda: {{ $product->name }}</p>
+             <p>Kolicina: {{ $item['amount'] }}</p>
+         @endif
+     @endforeach
+
+
+
  @endforeach
 
 @endsection
